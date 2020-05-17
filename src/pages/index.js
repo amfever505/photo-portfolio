f2409cc86efec6e09b40d4087101c823300af3fd
Kasumi <amfever@chenmianmiandeMacBook-Air.local>
2020-05-17 20:59:04 +0900

    init project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+import "../assets/css/font.css"
+import Header from "../components/header"
+
+export default function Home() {
+  return (
+    <div style={{ color: "#666" }}>
+      <Header headerText="Kasumi's portfolio" />
+      <p>かすみのポートフォリオ</p>
+      <Link to="/about/">click to about me</Link>
+    </div>
+  )
+}
