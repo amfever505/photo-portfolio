f4844b1f3111db94c352527ff7177aedfd0c432d
Kasumi <amfever@chenmianmiandeMacBook-Air.local>
2020-05-17 17:48:42 +0900

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-hello-world.git)

---
+++ src/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "../assets/css/font.css"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: "#666" }}>
+      <Header headerText="About" />
+
+      <p>私はかすみと申します。</p>
+    </div>
+  )
+}
